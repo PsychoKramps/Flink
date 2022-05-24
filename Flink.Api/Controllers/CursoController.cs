4acec46314d2f1ee8bd5b54925c3ad41619290c5
yava.lopez1 <you@example.com>
2022-05-24 18:58:50 -0500

    cambio en el appsetting.json

---
+++ Flink.Api/Controllers/CursoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flink.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+    }
+}
