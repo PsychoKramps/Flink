59621ca43fee53e5ed87ebd5972e9e60b3e0170c
PsychoKramps <Fraiden70@gmail.com>
2022-05-24 19:00:23 -0500

    Commit Usuario

---
+++ Flink.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flink.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    
+    public class UsuarioController : ControllerBase
+    {
+
+
+
+    }
+}
